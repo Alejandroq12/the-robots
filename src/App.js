f8027b1e552858fb994c2cae4321ae91d674aa4c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-07-17 13:13:13 -0600

    Feat: Add search box.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
     <>
       <h1>The Robots</h1>
+      <SearchBox />
       <CardList robots={robots} />
     </>
   );
