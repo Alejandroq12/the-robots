d0a8437187d02de0de3c651600ba99b908390487
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-07-29 13:13:13 -0600

    Fix: Add Redux-loger function call.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './containers/App';
 import './index.css';
 import { searchRobots } from './reducers';
 
-const logger = createLogger;
+const logger = createLogger();
 const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
