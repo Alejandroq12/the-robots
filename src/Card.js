f92adfd8c4f3b47a15f7f986572e2df33a890130
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-08-06 12:34:57 -0600

    Feat: create card component.

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+
+const Card = () => {
+  return (
+    <div>
+      <h1>The Robots</h1>
+      <img alt='Robots photo' src='https://robohash.org/test?size=200x200' />
+      <div>
+        <h2>Jane Doe</h2>
+        <p>jane.doe@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
